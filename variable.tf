b9efe1a1e421ae42ccb04a5abbf2c50ca7418c9f
Ayush-bhoyar <ayush2bhoyar@gmail.com>
2025-07-22 19:24:32 +0530

    Created the root variable.tf
    This is root module variable.tf file

---
+++ variable.tf
@@ -0,0 +1,21 @@
+variable "main_cidr_block" {
+  type = string
+}
+
+variable "public_subnet_count" {
+  type = number
+}
+
+variable "private_subnet_count" {
+  type = number
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+
+
+
+
+
